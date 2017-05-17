b0ab47d1c22eea269d66d53851e3486f3185aeb1
skuirrel0311 <rureikus7223@gmail.com>
2017-05-17 21:02:05 +0900

    リファクタリングの内容を明記したテキストを追加。

---
+++ FibonacciNumbers/FibonacciNumbers_First.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+void showFibonacciNumbers(int num);
+int getFibonacciNumber(int num);
+
+int main()
+{
+	const int num = 16;
+
+	showFibonacciNumbers(num);
+}
+
+void showFibonacciNumbers(int num)
+{
+	for (int i = 0; i <= num; i++)
+	{
+		getFibonacciNumber(i);
+		std::cout << getFibonacciNumber(i) << " ";
+	}
+}
+
+int getFibonacciNumber(int num)
+{
+	//1つ前
+	int buf1 = 0;
+	//2つ前
+	int buf2 = 0;
+
+	int result = 0;
+
+	for (int i = 0; i <= num; i++)
+	{
+		if (i <= 1) result = 1;
+		else result = buf1 + buf2;
+
+		buf2 = buf1;
+		buf1 = num;
+	}
+
+	return result;
+}
---
+++ FizzBazz/FizzBazz_First.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+void showFizzBazz(int num);
+std::string getFizzBazz(int num);
+
+int main()
+{
+	const int num = 16;
+
+	showFizzBazz(num);
+}
+
+void showFizzBazz(int num)
+{
+	for (int i = 1; i <= num; i++)
+	{
+		std::cout << getFizzBazz(i) << '\n';
+	}
+}
+
+std::string getFizzBazz(int num)
+{
+	std::string result;
+
+	if (num % 3 == 0) result += "Fizz";
+	if (num % 5 == 0) result += "Bazz";
+
+	if (result.empty()) return std::to_string(num);
+
+	return result;
+}
