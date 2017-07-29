aa1b63e088201e17fff26f2543a94291269921ed
skuirrel0311 <rureikus7223@gmail.com>
2017-07-30 01:16:48 +0900

    新規プロジェクト追加

---
+++ KanekoUtilitiesProject/Assets/KanekoUtilities/Programmer/Utilities/IsRendered.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace KKUtilities
+{
+    public class IsRendered : MonoBehaviour
+    {
+        public bool isRendered { get; private set; }
+
+        void Update()
+        {
+            isRendered = false;
+        }
+
+        void OnWillRenderObject()
+        {
+            if (Camera.current.name == "MainCamera")
+            {
+                isRendered = true;
+            }
+        }
+    }
+}
